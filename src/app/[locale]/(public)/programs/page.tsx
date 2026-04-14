f01fed95f882d02652ade8e50ac51ade10540c97
Muhammad-Jibril B.A. (Khalifa MBA) <jbashir52@gmail.com>
2026-04-14 12:37:55 +0200

    Added the donation link

--- src/app/[locale]/(public)/programs/page.tsx
+++ src/app/[locale]/(public)/programs/page.tsx
@@ -24,6 +24,7 @@ export default async function ProgramsPage({
     duration?: string
     intake?: string
     scholarship?: string
+    page?: string
   }
 }) {
   const query = normalizeSearchQuery(searchParams?.query)
